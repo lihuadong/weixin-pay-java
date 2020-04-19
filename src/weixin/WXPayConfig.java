ba1eb9f7d94762de2fbc2ed8725886800dd67fa9
lihuadong <lihuadongv@163.com>
2020-04-19 23:50:49 +0800

    配置文件备份方案

--- src/weixin/WXPayConfig.java
+++ src/weixin/WXPayConfig.java
@@ -1,7 +1,14 @@
 package weixin;
 
-public class Test {
+public class WXPayConfig {
 
+	public static String  APPID="wxff5be05cad2b9a89";
+	public static String APPSECRET="6cf98e501c110ca7db00be96ece98fc7";
+	public static String MCHID="10010723";
+	public static String KEY="ipaibanipaibanipaibanipaiban1234"; 
+	public static String NOTIFY_URL="http://ipaiban.com/wxpay/notify.jsp";
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi,weixin-pay-java");
