8f1d2b8c57c09c87588f17f81ea766218ad3c6c0
lihuadong <lihuadongv@163.com>
2020-04-28 23:33:01 +0800

    update time mark

---
+++ src/weixin/pay/server/WechatPayConfig.java
@@ -0,0 +1,25 @@
+/**
+ * 包打听全知道-微信H5版本
+ * xiaochengxu.pay
+ * WechatPayConfig.java
+ * Ver0.0.1
+ * 2017年10月13日-下午12:48:53
+ *  2017全智道(北京)科技有限公司-版权所有
+ * 
+ */
+package weixin.pay.server;
+
+/**
+ * 
+ * WechatPayConfig
+ * 
+ * 李华栋
+ * 李华栋
+ * 2017年10月13日 下午12:48:53
+ * 
+ * @version 0.0.1
+ * 
+ */
+public class WechatPayConfig {
+
+}
